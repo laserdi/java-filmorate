ec2796978cd4e1f7756268e2de169bd8866339af
Dmitriy Gaju <v013v@ya.ru>
2022-09-26 07:30:30 +0300

    Sprint 12 Final. Final. It's worked. FilmoRateApplicationTest.java

--- src/test/java/ru/yandex/practicum/FilmoRateApplicationTest.java
+++ src/test/java/ru/yandex/practicum/FilmoRateApplicationTest.java
@@ -1,35 +1,12 @@
 package ru.yandex.practicum;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.model.User;
-import ru.yandex.practicum.storage.user.dao.UserStorage;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-class FilmoRateApplicationTest {
-    @Qualifier("UserDBStorage")
-    private final UserStorage userStorage;
+class FilmorateApplicationTests {
     
     @Test
-    public void testFindUserById() {
-        
-        Optional<User> userOptional = Optional.of(userStorage.getUserById(1));
-        
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
+    void contextLoads() {
     }
 }
-
