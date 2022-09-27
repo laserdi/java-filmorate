5377f711d6a09b62badbc170dd62a3d1197b3c3e
Dmitriy Gaju <v013v@ya.ru>
2022-09-27 15:41:27 +0300

    Sprint 12 Final. Final. It's worked. Удалил лишние сервисы.

--- src/main/java/ru/yandex/practicum/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/service/GenreService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class GenreService {
     @Qualifier("GenreDBStorage")
     private final GenreStorage genreStorage;
-    ValidationService validationService;
+    private final ValidationService validationService;
     
     public GenreService(GenreStorage genreStorage, ValidationService validationService) {
         this.genreStorage = genreStorage;
